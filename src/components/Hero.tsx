8c0e38fc7d101bee1516bac365c1811d642e2d65
Sabina Kadyrova <sabinakadyrova@Sabinas-MacBook-Pro.local>
2025-07-10 21:53:22 -0400

    added button-CrimeaVox

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -37,12 +37,22 @@ const Hero = () => {
             {t('hero.description2')}
           </p>
 
-          <div className="flex flex-col sm:flex-row gap-4 justify-center">
-            <button className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-4 rounded-lg font-semibold text-lg flex items-center justify-center gap-2 transition-all duration-300 transform hover:scale-105 hover:shadow-lg">
+          <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
+            <button className=" h-16 bg-orange-500 hover:bg-orange-600 text-white px-8 py-4 rounded-lg font-semibold text-lg flex items-center justify-center gap-2 transition-all duration-300 transform hover:scale-105 hover:shadow-lg">
               {t('hero.button_learn_more')}
               <ArrowRight className="h-5 w-5" />
             </button>
             
+            {/* <button onClick={() => {
+                console.log('Image button clicked')}}
+              className="transition-all duration-300 transform hover:scale-105 focus:outline-none"
+            >
+              <img
+                src="../images/partnersLogo/logo_01.png" 
+                alt={t('hero.button_learn_more')}
+                className="h-20 w-auto" 
+              />
+            </button> */}
             <button className="border-2 border-white text-white hover:bg-white hover:text-blue-900 px-8 py-4 rounded-lg font-semibold text-lg transition-all duration-300 transform hover:scale-105">
               {t('hero.button_media')}
             </button>
